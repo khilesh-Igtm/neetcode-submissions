0de57caf5269c532f7db2187d0aa1e94e3bc7643
khilesh singh <57244134+khilesh-Igtm@users.noreply.github.com>
2026-09-08 22:03:00 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length) return false;
+        let arr = new Array(26).fill(0);
+        for(let i =0;i<s.length;i++){
+            arr[s.charCodeAt(i) - 97]++;
+        }
+        for(let i =0;i<t.length;i++){
+            arr[t.charCodeAt(i)- 97]--;
+        }
+
+        for(let i =0;i<arr.length;i++){
+            if(arr[i] !== 0){
+                return false;
+            }
+        }
+
+return true;
+
+    }
+}
